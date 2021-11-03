e3c8f66516f06ecc81f1d9afead362b540a2245e
Yaojie Li <jeffrey.yaojie.li@gmail.com>
2021-11-02 23:57:03 -0400

    Update-Edge-BounceBehaviour-Models-Jeffrey-20211102

---
+++ projects/INFR1350U-MidtermProject/src/BounceBehaviour.cpp
@@ -0,0 +1,41 @@
+#include "BounceBehaviour.h"
+#include "Gameplay/Components/ComponentManager.h"
+#include "Gameplay/GameObject.h"
+
+BounceBehaviour::BounceBehaviour() :
+	IComponent(),
+	_renderer(nullptr)
+{ }
+BounceBehaviour::~BounceBehaviour() = default;
+
+void BounceBehaviour::OnEnteredTrigger(const Gameplay::Physics::TriggerVolume::Sptr& trigger) {
+	if (_renderer) {
+		std::cout << "TRIGGER!!! COUT by BounceBehaviour" << std::endl;
+	}
+}
+
+void BounceBehaviour::OnLeavingTrigger(const Gameplay::Physics::TriggerVolume::Sptr& trigger) {
+	LOG_INFO("Left trigger: {}", trigger->GetGameObject()->Name);
+}
+
+
+
+void BounceBehaviour::Awake() {
+	_renderer = GetComponent<RenderComponent>();
+}
+
+void BounceBehaviour::RenderImGui() { }
+
+
+nlohmann::json BounceBehaviour::ToJson() const {
+	return {
+		/*
+		{ "enter_material", EnterMaterial != nullptr ? EnterMaterial->GetGUID().str() : "null" },
+		{ "exit_material", ExitMaterial != nullptr ? ExitMaterial->GetGUID().str() : "null" }*/
+	};
+}
+
+BounceBehaviour::Sptr BounceBehaviour::FromJson(const nlohmann::json& blob) {
+	BounceBehaviour::Sptr result = std::make_shared<BounceBehaviour>();
+	return result;
+}
